de19ff4838d4594b8bfc41a8fd2ecb3b4ef83338
j23377前田快人 <j23377mk@edu.tuis.ac.jp>
2025-05-26 12:12:27 +0900

    kadai6-1.py

---
+++ kadai6-1.py
@@ -0,0 +1,24 @@
+import requests
+
+APP_ID = "ba71fbaffed8dd9dec6a3baae114960eee230fa4"
+API_URL  = "https://api.e-stat.go.jp/rest/3.0/app/json/getStatsData"
+
+params = {
+    "appId": APP_ID,
+    "statsDataId":"0003353944",
+    "metaGetFlg":"Y",
+    "cntGetFlg":"N",
+    "explanationGetFlg":"Y",
+    "annotationGetFlg":"Y",
+    "sectionHeaderFlg":"1",
+    "replaceSpChars":"0",
+    "lang": "J"  # 日本語を指定
+}
+
+
+
+#response = requests.get(API_URL, params=params)
+response = requests.get(API_URL, params=params)
+# Process the response
+data = response.json()
+print(data)
